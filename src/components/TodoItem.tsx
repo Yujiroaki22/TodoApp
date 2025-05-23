9c6f69ae9c4f0180619c44f31a3033f1ab7ee218
jake <zarniheinzaz@gmail.com>
2025-05-23 16:30:56 +0630

    add-hover

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -11,11 +11,11 @@ type TodoItemProps={
 const TodoItem: React.FC<TodoItemProps> = ({ todo, toogleComplete, deleteTodo }) => {
     return (
         <div className="p-4 border border-gray-200 rounded-md shadow-md bg-black text-white">
-            <div className="flex items-center">
+            <div className="flex items-center hover:cursor-pointer" >
                 <div className="mr-2" onClick={() => toogleComplete(todo.id)}>
                     {todo.isCompleted ? "✅" : "⭕️"}
                 </div>
-                <span className={todo.isCompleted ? "line-through" : ""}>{todo.body}</span>
+                <span className={todo.isCompleted ? "line-through " : ""} >{todo.body}</span>
             </div>
             <button className="mt-2 px-4 py-2 bg-red-500 text-white rounded-md" onClick={() => deleteTodo(todo.id)}>Remove Todo</button>
         </div>
